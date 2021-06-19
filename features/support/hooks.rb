74664bcbf34d1faa80c6113f6c7c028d6ca469db
Yusha Mariak <yushamariak@gmail.com>
2021-06-18 22:14:40 -0300

    arquivo hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,7 +1,14 @@
-Before do
-    puts ''
-end 
-
-After do
-    puts ''
+After do |scenario|
+    scenario_name = scenario.name.gsub(/\s+/,'_').tr('/','_')
+    foto = "reports/screenshots/#{scenario_name}.png"
+    page.save_screenshot(foto)
+    attach(foto, 'image/png')
 end
+
+#Before do
+#puts ''
+#end 
+
+#After do
+#    puts ''
+#end
